f6480e896f21aa4d7bf42342704e73fd211d2fc7
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-04-21 14:22:43 -0300

    feat: button scroll to top

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 const btnMobile = document.querySelector(".btn-modal");
 const listMobile = document.querySelector(".header__menu__links");
+const btnScrollToTop = document.querySelector(".btn-scroll-to-top");
 
 function toggleModal() {
   btnMobile.classList.toggle("active");
   listMobile.classList.toggle("active");
 }
 
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+
 btnMobile.addEventListener("click", toggleModal);
+btnScrollToTop.addEventListener("click", scrollToTop);
